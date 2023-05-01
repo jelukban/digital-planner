7b0675ab13dae8f668bafb4bd48e9f2421e32289
Jerianne Lukban <jeriannelukban19@gmail.com>
2023-04-30 19:30:36 -0700

    Created index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from "./App.js";
+import ReactDOM from "react-dom/client";
+
+const rootElement = document.getElementbyId("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
